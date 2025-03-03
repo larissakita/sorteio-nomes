c16ebabad2b30d501e3f572b42c24333c9bc5d78
Larissa Miuki Kitayama <larissakitazuki@gmail.com>
2025-03-03 18:09:23 -0300

    Implementação funcionalidades

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,39 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo(){
+ let valor = document.getElementById("amigo").value;
+ if (valor == ""){
+    alert("Por favor, insira um nome.");
+ }else{
+    amigos.push(valor);
+    limparLista();
+    atualizarLista();
+ }
+
+}
+
+function limparLista(){
+    document.getElementById("amigo").value = "";
+    
+}
+
+
+function atualizarLista(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li); 
+    };
+}
+
+function sortearAmigo(){
+    if (amigos.length === 0){
+        alert("Adicione o nome de algum amigo!");
+    }else{
+        let numeroAleatorio = Math.floor(Math.random() * amigos.length);
+        document.getElementById("resultado").innerHTML = `O amigo sorteado foi ${amigos[numeroAleatorio]}`;
+
+    }
+}
